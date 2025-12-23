4058fabf3c808e4b3f7913c432b6465aacbeb1de
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-23 11:22:48 +0530

    Even Numbers in between Range

---
+++ day6/EvenNumbersInRange.java
@@ -0,0 +1,7 @@
+
+public class EvenNumbersInRange {
+    public static void main(String[] args) {
+        System.out.println("Even Numbers In Range :");
+        for(int i = 1 ; i <= 20 ; i++)  if(i % 2 == 0)  System.out.print(i + "\t");
+    }
+}
