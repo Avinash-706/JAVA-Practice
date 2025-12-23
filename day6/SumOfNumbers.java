c16ad593f903e0ea78859b087492e61a9ea4baf3
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-23 11:29:55 +0530

    Sum of All Numbers From 1 to 10

---
+++ day6/SumOfNumbers.java
@@ -0,0 +1,8 @@
+public class SumOfNumbers{
+
+    public static void main(String[] args){
+        int res = 0;
+        for(int i = 1 ; i <= 10 ; i++)  res += i;
+        System.out.print("Sum of Numbers in Range 1 to 10 : " + res);
+    }
+}
