b2df667689678e4d2f6b7379af799c8ec7115246
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-23 12:37:12 +0530

    Perfect Numbers In A Range

---
+++ day6/PerfectNumberInRange.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PerfectNumberInRange {
+        public static boolean perfectCheck(int n) {
+        if(n <= 1)      return false;
+
+        int res = 1;
+        for(int i = 2 ; i < n ; i++)   if(n % i == 0)  res += i;
+
+        return  res == n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+
+        System.out.println("Perfect Numbers In Range 1 to " + n + "  :  ");
+        for(int i = 1; i <= n ; i++)    if(perfectCheck(i))     System.out.println(i + "\t:  Perfect Number ");
+    }
+}
