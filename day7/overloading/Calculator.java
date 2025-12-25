7c900d40642f319a9651cf4f3c8424fb6b50b96d
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-25 21:06:49 +0530

    Add, Sub, Multiply - 2, 3, 4 and 5 Numbers : Function Overloading

--- day7/overloading/Calculator.java
+++ day7/overloading/Calculator.java
@@ -1,25 +1,40 @@
 public class Calculator {
-    public static int add3(int a, int b, int c) {
-        return a + b + c;
-    }
 
-    public static int multiply3(int a, int b, int c) {
-        return a * b * c;
-    }
+    public static int add(int a, int b)                         {return a + b ;}
+    public static int add(int a, int b, int c)                  {return a + b + c ;}
+    public static int add(int a, int b, int c, int d)           {return a + b + c + d ;}
+    public static int add(int a, int b, int c, int d, int e)    {return a + b + c + d + e ;}
 
-    public static int subtract4(int a, int b, int c, int d) {
-        return a - b - c - d;
-    }
+    public static int subtract(int a, int b)                         {return a - b ;}
+    public static int subtract(int a, int b, int c)                  {return a - b - c ;}
+    public static int subtract(int a, int b, int c, int d)           {return a - b - c - d ;}
+    public static int subtract(int a, int b, int c, int d, int e)    {return a - b - c - d - e ;}
 
-    public static int add2(int a, int b) {
-        return a + b;
-    }
+    public static int multiply(int a, int b)                         {return a * b ;}
+    public static int multiply(int a, int b, int c)                  {return a * b * c ;}
+    public static int multiply(int a, int b, int c, int d)           {return a * b * c * d ;}
+    public static int multiply(int a, int b, int c, int d, int e)    {return a * b * c * d * e ;}
+
+    public static int divide(int a, int b)                           {return a / b;}
+
 
     public static void main(String[] args) {
-        System.out.println("Add 3 Numbers :\t" + add3(10, 20, 30));
-        System.out.println("Multiply 3 Numbers :\t" + multiply3(2, 3, 4));
-        System.out.println("Subtract 4 Numbers :\t" + subtract4(10, 3, 4, 2));
-        System.out.println("Add 2 Numbers :\t" + add2(10, 20));
+
+        System.out.println("\nAdd 2 Numbers :\t" +  add(10, 20));
+        System.out.println("Add 3 Numbers :\t" +    add(10, 20, 30));
+        System.out.println("Add 4 Numbers :\t" +    add(10, 20, 30, 40));
+        System.out.println("Add 5 Numbers :\t" +    add(10, 20, 30, 40, 50));
+
+        System.out.println("\nSubtract 2 Numbers :\t" + subtract(10, 3));
+        System.out.println("Subtract 3 Numbers :\t" +   subtract(10, 3, 4));
+        System.out.println("Subtract 4 Numbers :\t" +   subtract(10, 3, 4, 2));
+        System.out.println("Subtract 5 Numbers :\t" +   subtract(10, 3, 4, 2, 1));
+
+        System.out.println("\nMultiply 2 Numbers :\t" + multiply(2, 3));
+        System.out.println("Multiply 3 Numbers :\t" +   multiply(2, 3, 4));
+        System.out.println("Multiply 4 Numbers :\t" +   multiply(2, 3, 4, 5));
+        System.out.println("Multiply 5 Numbers :\t" +   multiply(2, 3, 4, 5, 6));
+
+        System.out.println("\nDivide 2 Numbers   :\t" + divide(4, 2));
     }
-    
 }
