83600bfd2a7ad3e28bff3c41b7768d48dd789e35
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-26 22:16:15 +0530

    Double to Int -- Explicit TypeCasting

---
+++ day9/typecasting/DoubleToInt.java
@@ -0,0 +1,9 @@
+public class DoubleToInt {
+
+    public static void main(String[] args) {
+        double b = 45.78;
+        int    a = (int)b;
+
+        System.out.println("Double to Int Type-Conversion : " + a); 
+    }
+}
